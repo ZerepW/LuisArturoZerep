2648e178ee208eb2dd3e2e33fdb693af411cb6d1
ZerepW <A01273687@itesm.mx>
2017-08-20 17:34:45 -0500

    FontAwesome

---
+++ Node.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var fs = require("fs");
+
+var app = express();
+
+app.get("/",function(req,res){
+    fs.readFile("index.html",function(err,data){
+        res.end(data); //manda y cierra la conexion
+    });
+});
+
+app.listen(80);   
