0674137182fff6241daffaa91ca861a8049d085f
ZerepW <A01273687@itesm.mx>
2017-08-20 14:59:58 -0500

    Agregando index

--- Node.js
+++ Node.js
@@ -1,9 +1,12 @@
 var express = require("express");
+var fs = require("fs");
 
 var app = express();
 
 app.get("/",function(req,res){
-    res.send("Hola!"); //manda y cierra la conexion
+    fs.readFile("index.html",function(err,data){
+        res.end(data); //manda y cierra la conexion
+    });
 });
 
-app.listen(7780);   
+app.listen(7781);   
