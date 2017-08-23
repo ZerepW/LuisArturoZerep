b373fb619b98b954f7d0deae6db78c4fe47d33d8
ZerepW <A01273687@itesm.mx>
2017-08-23 00:35:18 -0500

    Iconos de aplicación

--- Node.js
+++ Node.js
@@ -4,9 +4,7 @@ var fs = require("fs");
 var app = express();
 
 app.get("/",function(req,res){
-    fs.readFile("index.html",function(err,data){
-        res.end(data); //manda y cierra la conexion
-    });
+    res.sendFile("index.html",{root: __dirname});
 });
 
 app.listen(80);   
