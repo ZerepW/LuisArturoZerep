cbcd591e01d9171fcbbd1af79afd3ef75a439635
ZerepW <A01273687@itesm.mx>
2017-09-10 19:25:17 -0500

    Version 2

--- Node.js
+++ Node.js
@@ -2,8 +2,8 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname,'img')));
-app.get("/",function(req,res){
+app.use(express.static(path.join(__dirname,'public')));
+app.get("/",function(req,res){ 
     res.sendFile("index.html",{root: __dirname});
 });
 
